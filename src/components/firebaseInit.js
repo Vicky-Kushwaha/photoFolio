8cadfe956ce8be35a3b4f956ef781d4e89411e3b
Vicky Kumar <147392131+Vicky-Kushwaha@users.noreply.github.com>
2025-01-19 22:26:53 +0530

    Update firebaseInit.js

--- src/components/firebaseInit.js
+++ src/components/firebaseInit.js
@@ -6,12 +6,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD2C3oU0F_XlwAOgTjGKbPdQatMDoo2uQ0",
-  authDomain: "blogging-app-cf000.firebaseapp.com",
-  projectId: "blogging-app-cf000",
-  storageBucket: "blogging-app-cf000.firebasestorage.app",
-  messagingSenderId: "296561509167",
-  appId: "1:296561509167:web:5eeca916a7928afa13d7da",
+// add your own firebase config
 };
 
 // Initialize Firebase
